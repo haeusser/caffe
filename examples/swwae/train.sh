4eef964ae9b8db3b362d608b61b238cde358d650
Philip Häusser <haeusser@cs.tum.edu>
2015-07-17 10:50:04 +0200

    redirect log to logfile

--- examples/swwae/train.sh
+++ examples/swwae/train.sh
@@ -5,9 +5,12 @@
 # or similar path
 
 TOOLS=$CAFFE_ROOT/build/tools
+LOG_DIR=$CAFFE_ROOT/examples/swwae/log
 mkdir -p $CAFFE_ROOT/examples/swwae/modelfiles
+mkdir -p $LOG_DIR
 MODELFILE=$(ls $CAFFE_ROOT/examples/swwae/modelfiles -t | head -1)
 
 $TOOLS/caffe train \
     --solver=$CAFFE_ROOT/examples/swwae/solver.prototxt \
-    --snapshot=$CAFFE_ROOT/examples/swwae/modelfiles/$MODELFILE
+    --snapshot=$CAFFE_ROOT/examples/swwae/modelfiles/$MODELFILE \
+    2>&1 | tee $LOG_DIR/log.log
