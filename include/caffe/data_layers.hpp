a24f5bdffc391d922e977a825a56aa20c9dca810
Philipp Fischer <fischer@informatik.uni-freiburg.de>
2015-08-19 15:12:24 +0200

    Added ImgWriter

--- include/caffe/data_layers.hpp
+++ include/caffe/data_layers.hpp
@@ -261,6 +261,34 @@ class ImageDataLayer : public BasePrefetchingDataLayer<Dtype> {
 
 
 /**
+ * @brief ImgWriterLayer writes PPMs and PGMs
+ *
+ */
+template <typename Dtype>
+class ImgWriterLayer : public Layer<Dtype> {
+ public:
+  explicit ImgWriterLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual ~ImgWriterLayer() {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  
+  virtual inline const char* type() const { return "ImgWriter"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+ protected:
+
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
+};
+
+/**
  * @brief Phils Data Layer
  *
  */
