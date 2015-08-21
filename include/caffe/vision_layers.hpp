a6bd653537e1302c14a8d887ae47ab7b4725a47f
Philipp Fischer <fischer@informatik.uni-freiburg.de>
2015-08-21 11:44:04 +0200

    Added DownsampleLayer (compiles, untested)

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -364,6 +364,47 @@ class CuDNNConvolutionLayer : public ConvolutionLayer<Dtype> {
 };
 #endif
 
+
+/**
+ * @brief Phil's Downsample Layer
+ * Takes a blob and downsamples width and height to given size
+ */
+template <typename Dtype>
+class DownsampleLayer : public Layer<Dtype> {
+ public:
+  explicit DownsampleLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "Downsample"; }
+  virtual inline int MinBottomBlobs() const { return 1; }
+  virtual inline int MaxBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  int count_;
+  int num_;
+  int channels_;
+  int height_;
+  int width_;
+  
+  int top_width_;
+  int top_height_;
+};
+
+
 /**
  * @brief A helper for image operations that rearranges image regions into
  *        column vectors.  Used by ConvolutionLayer to perform convolution
