c1bf658f066ed749ff98d2215414309bd211ab11
Philip Haeusser <haeusser@cs.tum.edu>
2015-09-08 12:09:08 +0200

    sync commit

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -1,5 +1,5 @@
 from .pycaffe import Net, SGDSolver, NesterovSolver, Solver, AdamSolver
-from ._caffe import set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver, layer_type_list, set_solver_count, get_solver_from_string, P2PSync, set_logging_disabled
+from ._caffe import set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver, layer_type_list, set_solver_count, get_solver_from_string, P2PSync, set_logging_disabled, set_callback_iteration, set_pysolver
 from .proto.caffe_pb2 import TRAIN, TEST
 from .classifier import Classifier
 from .detector import Detector
