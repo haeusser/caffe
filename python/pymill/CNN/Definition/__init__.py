c9d39de2e1d9f4f100cbe741a23445b5440236a3
Eddy Ilg <ilg@cs.uni-freiburg.de>
2015-09-17 12:33:41 +0200

    Added util.py

--- python/pymill/CNN/Definition/__init__.py
+++ python/pymill/CNN/Definition/__init__.py
@@ -4,11 +4,3 @@ from CaffeAdapter import *
 from Blocks import Blocks
 from BinaryData import BinaryData
 
-def writeImage(net, blob, folder='output', suffix=None, scale=1.0):
-    pass
-
-def writeFlow(net, blob, folder='output', suffix=None, scale=1.0):
-    pass
-
-def writeFloat(net, blob, folder='output', suffix=None, scale=1.0):
-    pass
