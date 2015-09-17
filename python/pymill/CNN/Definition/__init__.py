c234a91cb9f00a96ba994148ad3979857b2d9222
Eddy Ilg <ilg@cs.uni-freiburg.de>
2015-09-17 11:49:58 +0200

    added net parameter

--- python/pymill/CNN/Definition/__init__.py
+++ python/pymill/CNN/Definition/__init__.py
@@ -4,11 +4,11 @@ from CaffeAdapter import *
 from Blocks import Blocks
 from BinaryData import BinaryData
 
-def writeImage(blob, folder='output', suffix=None, scale=1.0):
+def writeImage(net, blob, folder='output', suffix=None, scale=1.0):
     pass
 
-def writeFlow(blob, folder='output', suffix=None, scale=1.0):
+def writeFlow(net, blob, folder='output', suffix=None, scale=1.0):
     pass
 
-def writeFloat(blob, folder='output', suffix=None, scale=1.0):
+def writeFloat(net, blob, folder='output', suffix=None, scale=1.0):
     pass
