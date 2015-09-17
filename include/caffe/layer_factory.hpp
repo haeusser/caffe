361f19775ac59aab704f15c269b22512fcd9406a
Eddy Ilg <ilg@cs.uni-freiburg.de>
2015-09-17 11:37:40 +0200

    added layer name in LOG() code

--- include/caffe/layer_factory.hpp
+++ include/caffe/layer_factory.hpp
@@ -73,7 +73,7 @@ class LayerRegistry {
   // Get a layer using a LayerParameter.
   static shared_ptr<Layer<Dtype> > CreateLayer(const LayerParameter& param) {
     if (Caffe::root_solver()) {
-      LOG(INFO) << "Creating layer " << param.name();
+      LOG(INFO) << "Creating layer " << param.name() << " of type " << param.type();
     }
     const string& type = param.type();
     CreatorRegistry& registry = Registry();
