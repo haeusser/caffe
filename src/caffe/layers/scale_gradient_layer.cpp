88b112e00be583e62811fb5f3eea55ca9ca73a39
Alexey Dosovitskiy <dosovits@cs.uni-freiburg.de>
2015-10-09 12:16:35 +0200

    started SceneNet test, also crop_simple and scale_gradient layers

---
+++ src/caffe/layers/scale_gradient_layer.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+
+#include "caffe/layer.hpp"
+#include "caffe/util/math_functions.hpp"
+#include "caffe/vision_layers.hpp"
+
+namespace caffe {
+  
+template <typename Dtype>
+void ScaleGradientLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  discount_coeff_schedule_ = this->layer_param_.coeff_schedule_param();
+  iter_ = 0;
+}
+
+template <typename Dtype>
+void ScaleGradientLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  top[0]->Reshape(bottom[0]->num(), bottom[0]->channels(), bottom[0]->height(), bottom[0]->width());
+}
+
+template <typename Dtype>
+void ScaleGradientLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  top[0]->ShareData(*bottom[0]);
+}
+
+template <typename Dtype>
+void ScaleGradientLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  iter_++;
+
+  Dtype coeff = 1;
+  if (discount_coeff_schedule_.initial_coeff() == 0) {
+    if (discount_coeff_schedule_.final_coeff() == 0)
+      coeff = 0;
+    else
+      discount_coeff_schedule_.set_initial_coeff(1e-10);
+  } else  
+    coeff = discount_coeff_schedule_.initial_coeff() *
+                exp(( log(discount_coeff_schedule_.final_coeff() /discount_coeff_schedule_.initial_coeff()) ) *
+                (Dtype(2) / (Dtype(1) + exp(- 1.0986 * iter_ / discount_coeff_schedule_.half_life())) - Dtype(1)));
+                
+  caffe_cpu_axpby<Dtype>(top[0]->count(), coeff, top[0]->cpu_diff(),
+    Dtype(0), bottom[0]->mutable_cpu_diff());
+}
+  
+#ifdef CPU_ONLY
+STUB_GPU(ScaleGradientLayer);
+#endif
+
+INSTANTIATE_CLASS(ScaleGradientLayer);
+REGISTER_LAYER_CLASS(ScaleGradient);
+
+}  // namespace caffe
