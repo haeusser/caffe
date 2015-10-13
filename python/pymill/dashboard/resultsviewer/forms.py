b798c4472ab0510f65919a76d4124514103b74b7
Philip Haeusser <haeusser@cs.tum.edu>
2015-10-13 16:03:35 +0200

    dashboard: implemented "only last iteration" (known bug: sorting the table will clear filter)

--- python/pymill/dashboard/resultsviewer/forms.py
+++ python/pymill/dashboard/resultsviewer/forms.py
@@ -1,19 +1,21 @@
 from django import forms
 
 
-class MeasureSelectorForm(forms.Form):
+class OptionsForm(forms.Form):
     def __init__(self, *args, **kwargs):
-        if 'selected_measure' in kwargs:
-            self.selection = kwargs.pop('selected_measure')
-        else:
-            self.selection = 'flow_epe'
-        super(MeasureSelectorForm, self).__init__(*args, **kwargs)
+        self.measure_selection = kwargs.pop('selected_measure') if 'selected_measure' in kwargs else 'all'
+        self.only_last_iteration = kwargs.pop('only_last_iteration') if 'only_last_iteration' in kwargs else False
+
+
+        super(OptionsForm, self).__init__(*args, **kwargs)
 
         self.measures = (
                     ('all', 'all'),
                     ('flow_epe', 'flow_epe'),
+                    ('epe', 'epe'),
                     ('disp_epe', 'disp_epe'),
                     ('disp_change_epe', 'disp_change_epe'),
                     ('disp_change_err', 'disp_change_err')
                     )
-        self.fields['selected_measure'] = forms.ChoiceField(label='Error Measure', choices=self.measures, initial=self.selection)
+        self.fields['selected_measure'] = forms.ChoiceField(label='Error Measure', choices=self.measures, initial=self.measure_selection)
+        self.fields['only_last_iteration'] = forms.BooleanField(label='Last Iteration', required=False, initial=self.only_last_iteration)
--- python/pymill/dashboard/resultsviewer/views.py
+++ python/pymill/dashboard/resultsviewer/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from django_tables2   import RequestConfig
 from resultsviewer.models  import Results
 from resultsviewer.tables  import ResultsTable
-from .forms import MeasureSelectorForm
+from .forms import OptionsForm
 def results(request):
     queryset = Results.objects.using('results')
-    table = ResultsTable(queryset.all())
-
 
     best = queryset.order_by('value')[0]
     best_net = best.networkname
@@ -16,24 +14,29 @@ def results(request):
 
     measures = [x.values() for x in queryset.all().values('measure').distinct()]
 
-    RequestConfig(request, paginate=False).configure(table)
-
     if request.method == 'GET':
-        measure_selector_form = MeasureSelectorForm()
+        measure_selector_form = OptionsForm()
         selected_measure = 'none'
 
     if request.method == 'POST':
+        selected_options = request.POST
         if 'selected_measure' in request.POST:
             selected_measure = request.POST['selected_measure']
-            measure_selector_form = MeasureSelectorForm(request.POST)
+            measure_selector_form = OptionsForm(request.POST)
             if not request.POST['selected_measure'] == 'all':
-                table = ResultsTable(queryset.filter(measure=selected_measure).all())
+                queryset = queryset.filter(measure=selected_measure)
                 #table.exclude += ('measure',)
-            else:
-                table = ResultsTable(queryset.all())
+        if 'only_last_iteration' in request.POST and request.POST['only_last_iteration'] == 'on':
+            selected_options = "ONLY_LAST!"
+            ids = []
+            nets = [x.values() for x in queryset.all().values('networkname').distinct()]
+            for net in nets:
+                ids.append(queryset.filter(networkname=net[0]).order_by('-iteration').values()[0]['id'])
+            queryset = queryset.filter(id__in=ids)
 
-
+    table = ResultsTable(queryset.all(), order_by='value')
+    RequestConfig(request, paginate=False).configure(table)
     table.exclude += ('id',)
-    table.order_by = 'value'
+    #table.order_by = 'value'
 
     return render(request, 'results.html', locals()) # {'table': table})
