beacf7fce740e96426343e6fd47d2e6564f9f751
Philip Haeusser <haeusser@cs.tum.edu>
2015-10-14 18:32:42 +0200

    dashboard: added filter "position"

--- python/pymill/dashboard/resultsviewer/forms.py
+++ python/pymill/dashboard/resultsviewer/forms.py
@@ -3,26 +3,36 @@ from django import forms
 
 class OptionsForm(forms.Form):
     def __init__(self, *args, **kwargs):
-        self.measure_selection = kwargs.pop('selected_measure') if 'selected_measure' in kwargs else 'all'
         self.only_last_iteration = kwargs.pop('only_last_iteration') if 'only_last_iteration' in kwargs else False
-        self.measures = self.make_measures_list(kwargs.pop('measures')) if 'measures' in kwargs else\
+        self.measure_selection = kwargs.pop('selected_measure') if 'selected_measure' in kwargs else ''
+        self.measures = self.make_dropdown_list(kwargs.pop('measures')) if 'measures' in kwargs else\
                         (
-                            ('all', 'all'),
+                            ('', 'all'),
                             ('flow_epe', 'flow_epe'),
                             ('epe', 'epe'),
                             ('disp_epe', 'disp_epe'),
                             ('disp_change_epe', 'disp_change_epe'),
                             ('disp_change_err', 'disp_change_err')
                         )
+        self.position_selection = kwargs.pop('selected_position') if 'selected_position' in kwargs else ''
+        self.positions =self.make_dropdown_list(kwargs.pop('positions')) if 'positions' in kwargs else\
+                        (
+                            ('', 'all'),
+                            ('L', 'L'),
+                            ('R', 'R'),
+                            ('0L', '0L'),
+                            ('0R', '0R'),
+                        )
 
         super(OptionsForm, self).__init__(*args, **kwargs)
 
-
-        self.fields['selected_measure'] = forms.ChoiceField(label='Error Measure', choices=self.measures, initial=self.measure_selection)
+        self.fields['selected_measure'] = forms.ChoiceField(label='Error Measure', choices=self.measures, initial=self.measure_selection, required=False)
+        self.fields['selected_position'] = forms.ChoiceField(label='Position', choices=self.positions, initial=self.position_selection, required=False)
         self.fields['only_last_iteration'] = forms.BooleanField(label='Last Iteration', required=False, initial=self.only_last_iteration)
 
-    def make_measures_list(self, measures):
-        result = ('all', 'all'),
+    def make_dropdown_list(self, measures):
+        result = ('', 'all'),
         for m in measures:
-            result += (m[0], m[0]),
+            if m not in [[''], [None]]:
+                result += (m[0], m[0]),
         return result
--- python/pymill/dashboard/resultsviewer/views.py
+++ python/pymill/dashboard/resultsviewer/views.py
@@ -10,13 +10,16 @@ def results(request):
     queryset = Results.objects.using('results')
 
     measures = [x.values() for x in queryset.all().values('measure').distinct()]
+    positions = [x.values() for x in queryset.all().values('position').distinct()]
 
     filter_params = dict()
-    if 'order_by' in json.loads(request.COOKIES.get('filter_params')):
-        filter_params['order_by'] = json.loads(request.COOKIES.get('filter_params'))['order_by']
+
 
     cookie_set = True if 'filter_params' in request.COOKIES else False  # TODO check age of cookie!
     new_cookie_necessary = False
+
+    if cookie_set and 'order_by' in json.loads(request.COOKIES.get('filter_params')):
+        filter_params['order_by'] = json.loads(request.COOKIES.get('filter_params'))['order_by']
 
     if request.method == 'POST' or cookie_set:
         if request.method == 'POST':
@@ -30,10 +33,14 @@ def results(request):
 
         if 'selected_measure' in filter_params:
             selected_measure = filter_params['selected_measure']
-            measure_selector_form = OptionsForm(filter_params, measures=measures)
-            if not filter_params['selected_measure'] == 'all':
+            if not filter_params['selected_measure'] == '':
                 queryset = queryset.filter(measure=selected_measure)
                 # table.exclude += ('measure',)
+
+        if 'selected_position' in filter_params:
+            selected_position = filter_params['selected_position']
+            if not filter_params['selected_position'] == '':
+                queryset = queryset.filter(position=selected_position)
 
         if 'only_last_iteration' in filter_params and filter_params['only_last_iteration'] == 'on':
             ids = []
@@ -48,7 +55,6 @@ def results(request):
             queryset = queryset.filter(id__in=ids)
 
     if request.method == 'GET':
-        measure_selector_form = OptionsForm(filter_params, measures=measures)
         selected_measure = 'none'
         if 'sort' in request.GET:
             filter_params['order_by'] = request.GET['sort']
@@ -59,15 +65,18 @@ def results(request):
     RequestConfig(request, paginate=False).configure(table)
     table.exclude += ('id',)
 
-    best = queryset.order_by('value')[0]
-    best_net = best.networkname
-    best_dataset = best.dataset
-    best_value = best.value
-    best_measure = best.measure
+    try:  # possibly, the queryset is empty
+        best = queryset.order_by('value')[0]
+        best_net = best.networkname
+        best_dataset = best.dataset
+        best_value = best.value
+        best_measure = best.measure
+    except:
+        pass
+
+    filter_form = OptionsForm(filter_params, measures=measures, positions=positions)
 
     response = render(request, 'results.html', locals())
-
     if new_cookie_necessary:
         response.set_cookie(key='filter_params', value=json.dumps(filter_params))
-
     return response
